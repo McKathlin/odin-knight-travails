d0c03f60353b70fb66cefad6e8d267407a0ada47
McKathlin <mckathlin@gmail.com>
2025-01-07 20:04:23 +0000

    Test Queue

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 import knightMoves from './knight.js';
+import Queue from './queue.js';
+import test from './test.js';
+
+// Queue test
+let q = new Queue([9, 18, 27, 36]);
+q.enqueue(10);
+test.areEqual(q.dequeue(), 9);
+q.enqueue(20);
+test.areEqual(q.dequeue(), 18);
+test.areEqual(q.dequeue(), 27);
+q.enqueue(30);
+test.areEqual(q.dequeue(), 36);
+test.areEqual(q.dequeue(), 10);
+test.areEqual(q.dequeue(), 20);
+test.areEqual(q.dequeue(), 30);
+test.areEqual(q.dequeue(), null);
 
 console.log(knightMoves([0,0], [1,2]));
 
--- test.js
+++ test.js
@@ -1,20 +1,32 @@
 export default (function() {
   function areEqual(a, b, description) {
-    if (a == b) {
-      console.log(`Passed: ${description}`);
+    if (a === b) {
+      if (description) {
+        console.log(`Passed: ${description}`);
+      }
       return true;
     } else {
-      console.error(`Failed: ${description}`);
+      if (description) {
+        console.error(`Failed: ${description}`);
+      } else {
+        throw new Error("Equality test failed");
+      }
       return false;
     }
   }
 
   function isTrue(condition, description) {
     if (condition) {
-      console.log(`Passed: ${description}`);
+      if (description) {
+        console.log(`Passed: ${description}`);
+      }
       return true;
     } else {
-      console.error(`Failed: ${description}`);
+      if (description) {
+        console.error(`Failed: ${description}`);
+      } else {
+        throw new Error("Assertion test failed");
+      }
       return false;
     }
   }
