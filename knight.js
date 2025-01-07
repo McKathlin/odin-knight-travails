c77c16101bdbfdcaa621497a2c88349519eb4537
McKathlin <mckathlin@gmail.com>
2025-01-07 19:07:58 +0000

    Create package.json and script stubs

---
+++ knight.js
@@ -0,0 +1,7 @@
+function knightMoves(src, dest) {
+  console.log("This is the knightMoves function");
+  // TODO: implement
+  return [src, dest];
+}
+
+export default knightMoves;
---
+++ main.js
@@ -0,0 +1,5 @@
+import knightMoves from './knight.js';
+
+console.log(knightMoves([0,0], [1,2]));
+
+// TODO: test the knightMoves function
