eff5b623d859d37a4592ba9ecbc28ca05a5342dc
McKathlin <mckathlin@gmail.com>
2025-01-07 19:26:11 +0000

    Add helper method stubs

--- knight.js
+++ knight.js
@@ -1,7 +1,25 @@
 function knightMoves(src, dest) {
   console.log("This is the knightMoves function");
-  // TODO: implement
-  return [src, dest];
+  let path = [src];
+  let visited = new Set();
+
+  return path;
+}
+
+function _forEachNextStep([xStart, yStart], callback) {
+  // TODO
+  // Perform the callback on each x,y coordinate pair
+  // that represents a direct step from srcCoords.
+}
+
+function _areCoordsEqual([xa, ya], [xb, yb]) {
+  return xa == xb && ya == yb;
+}
+
+// All board indices have exactly one digit,
+// so a coordinate pair can be converted to a two-char string unambiguously.
+function _coordsToKey([x, y]) {
+  return `${x}${y}`;
 }
 
 export default knightMoves;
