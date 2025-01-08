f00a716cee3ba78f89ceb5131c37e25589639a51
McKathlin <mckathlin@gmail.com>
2025-01-08 20:55:03 +0000

    Wrap knight.js in IIFE and implement KeySet

--- knight.js
+++ knight.js
@@ -1,28 +1,67 @@
 import Queue from "./queue.js";
 
-function knightMoves(src, dest) {
-  console.log("This is the knightMoves function");
-  let path = [src];
-  let visited = new Set();
-  let queue = new Queue();
-  // TODO: implement
-  return path;
-}
+export default (function() {
+  function knightMoves(src, dest) {
+    console.log("This is the knightMoves function");
+    let pathQueue = new Queue();
+    pathQueue.enqueue([src]);
+    let visitedCoords = new KeySet(_coordsToKey);
+    visitedCoords.add(src);
+    while (!pathQueue.isEmpty()) {
+      let currentPath = pathQueue.dequeue();
+      let latestCoords = currentPath[currentPath.length - 1];
+      _forEachNextStep(latestCoords, (x, y) => {
+        let nextPath = [...latestCoords, [x, y]];
+        // TODO: finish implementing
+      })
+    }
+    // TODO: Replace with actual path
+    return [src, dest];
+  }
 
-function _forEachNextStep([xStart, yStart], callback) {
-  // TODO
-  // Perform the callback on each x,y coordinate pair
-  // that represents a direct step from srcCoords.
-}
+  function _forEachNextStep([x, y], callback) {
+    // TODO
+  }
 
-function _areCoordsEqual([xa, ya], [xb, yb]) {
-  return xa == xb && ya == yb;
-}
+  function _coordsToKey(px, py) {
+    let x, y;
+    if (!y && Array.isArray(px)) {
+      [x, y] = px;
+    } else {
+      x = px;
+      y = py;
+    }
+    return `${x}${y}`;
+  }
+
+  class KeySet {
+    constructor(funcMakeKey) {
+      this.makeKey = funcMakeKey;
+      this._keySet = new Set();
+    }
+
+    add(value) {
+      return this._keySet.add(this.makeKey(value));
+    }
+
+    delete(value) {
+      return this._keySet.delete(this.makeKey(value));
+    }
+
+    has(value) {
+      return this._keySet.has(this.makeKey(value));
+    }
+  }
+
+  return knightMoves;
+}());
+
+
+
+
+
+
+
 
 // All board indices have exactly one digit,
 // so a coordinate pair can be converted to a two-char string unambiguously.
-function _coordsToKey([x, y]) {
-  return `${x}${y}`;
-}
-
-export default knightMoves;
