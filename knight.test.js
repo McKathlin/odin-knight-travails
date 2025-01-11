25e9874359e4125eb9a894271809efca5df28f56
McKathlin <mckathlin@gmail.com>
2025-01-11 19:46:30 +0000

    Create knight.test.js as stub

---
+++ knight.test.js
@@ -0,0 +1,34 @@
+import knightMoves from "./knight";
+
+test('Typical case', () => {
+  expect(true).toBe(false); // TODO: write test
+});
+
+test('makes size 1 route to itself', () => {
+  expect(true).toBe(false); // TODO: write test
+});
+
+test('makes size 2 route 1 move away', () => {
+  expect(true).toBe(false); // TODO: write test
+});
+
+test('cannot move outside of board', () => {
+  expect(true).toBe(false); // TODO: write test
+});
+
+test('moves along edge', () => {
+  expect(true).toBe(false); // TODO: write test
+});
+
+test('moves across board', () => {
+  expect(true).toBe(false); // TODO: write test
+});
+
+test('moves one square over', () => {
+  expect(true).toBe(false); // TODO: write test
+});
+
+test('moves between any pair of random positions', () => {
+  expect(true).toBe(false); // TODO: write test
+});
+
