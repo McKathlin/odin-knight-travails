0ef47ff8d75f302bc2419ad6dd60735704555c04
McKathlin <mckathlin@gmail.com>
2025-01-11 20:05:22 +0000

    Write knightMoves tests in knight.test.js

--- knight.test.js
+++ knight.test.js
@@ -1,34 +1,45 @@
 import knightMoves from "./knight";
 
-test('Typical case', () => {
-  expect(true).toBe(false); // TODO: write test
+test('typical two-move case', () => {
+  expect(knightMoves([4,5], [7, 2]).length).toBe(3);
+});
+
+test('matches start and end inputs', () => {
+  const result = knightMoves([4,5], [7, 2]);
+  expect(result[0]).toStrictEqual([4, 5]);
+  expect(result[result.length - 1]).toStrictEqual([7, 2]);
 });
 
 test('makes size 1 route to itself', () => {
-  expect(true).toBe(false); // TODO: write test
+  expect(knightMoves([5,2], [5,2]).length).toBe(1);
 });
 
 test('makes size 2 route 1 move away', () => {
-  expect(true).toBe(false); // TODO: write test
+  expect(knightMoves([3,3], [4,5]).length).toBe(2);
 });
 
-test('cannot move outside of board', () => {
-  expect(true).toBe(false); // TODO: write test
+test('cannot move to negative indices', () => {
+  expect(knightMoves([0,0], [-1,-2])).toBe(null);
+});
+
+test('cannot move beyond board size', () => {
+  expect(knightMoves([0,0], [8,8])).toBe(null);
 });
 
 test('moves along edge', () => {
-  expect(true).toBe(false); // TODO: write test
+  expect(!!knightMoves([7,0], [7,7])).toBe(true);
 });
 
 test('moves across board', () => {
-  expect(true).toBe(false); // TODO: write test
+  expect(!!knightMoves([7,7], [0,0])).toBe(true);
 });
 
 test('moves one square over', () => {
-  expect(true).toBe(false); // TODO: write test
+  expect(!!knightMoves([0,0], [0,1])).toBe(true);
 });
-
+/*
 test('moves between any pair of random positions', () => {
   expect(true).toBe(false); // TODO: write test
 });
+*/
 
